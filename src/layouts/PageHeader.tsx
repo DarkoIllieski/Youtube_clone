903d667c49636bd54e1178974c886a2a921b01f3
Darko <dj_mamlyooo@hotmail.com>
2023-12-13 22:21:21 +0100

    secondary colors added

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,10 +1,13 @@
 import logo from "../assets/logo.png"
+import { Menu } from "lucide-react"
 
 const PageHeader = () => {
   return (
     <div className="flex gap-10 lg:gap-20 justify-between">
       <div className="flex gap-4 items-center flex-shrink-0">
-        <button></button>
+        <button>
+          <Menu />
+        </button>
         <a href="./">
           <img src={logo} className="h-6"></img>
         </a>
