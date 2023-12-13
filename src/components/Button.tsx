d0331f2a08bb15b52a6b1271ae6c59fa49c62194
Darko <dj_mamlyooo@hotmail.com>
2023-12-13 22:22:14 +0100

    Button created

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import {cva } from "class-variance-authority"
+
+const buttonStyles = cva(["hover:bg-secondary-hover"]) 
+
+const Button = () => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
