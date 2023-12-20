73651746ad2a893d26fe6e99f78c6bba68fbd5ba
Darko <dj_mamlyooo@hotmail.com>
2023-12-20 23:31:43 +0100

    Buttons in a CategoryPills created

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Button from "./Button";
 
 const CategoryPills = () => {
-  return <div className="overflow-x-hodden relative">CategoryPills</div>;
+  return (
+    <div className="overflow-x-hodden relative">
+      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+        <Button variant="dark" className="py-1 px-3 rounded-lg whitespace-nowrap">All</Button>
+        <Button className="py-1 px-3 rounded-lg whitespace-nowrap">JavaScript</Button>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryPills;
