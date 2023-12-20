d3f8b06b5257e6674aa3eb5df917c52674f9a19a
Darko <dj_mamlyooo@hotmail.com>
2023-12-21 00:15:28 +0100

    CategoryPillProps updated selectedCategory/onSelect

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -2,16 +2,19 @@ import Button from "./Button";
 
 type CategoryPillProps = {
   categories: string[]
+  selectedCategory: string
+  onSelect: (category: string) => void
 }
 
-const CategoryPills = ({ categories }: CategoryPillProps) => {
+const CategoryPills = ({ categories, selectedCategory, onSelect }: CategoryPillProps) => {
   return (
     <div className="overflow-x-hodden relative">
       <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
       { categories.map(category => (
         <Button
         key={category}
-          variant="dark"
+        onClick={() => onSelect(category)}
+          variant={selectedCategory === category ? "dark" : "default"}
           className="py-1 px-3 rounded-lg whitespace-nowrap"
         >
           {category}
