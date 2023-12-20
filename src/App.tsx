7514d27598bdc99ef56624c8ef8030a67ab33387
Darko <dj_mamlyooo@hotmail.com>
2023-12-20 23:50:54 +0100

    CategoryPills added to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { PageHeader } from "./layouts/PageHeader.tsx";
 import CategoryPills from "./components/CategoryPills.tsx";
+import { categories } from "./data/home.ts";
 function App() {
   return (
-    <div>
+    <div className="max-h-screen flex flex-col">
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow overflow-auto">
         <div>Sidebar</div>
         <div className="sticky top-0 bg-white z-10 pb-4">
-          <CategoryPills />
+          <CategoryPills categories={categories}/>
         </div>
       </div>
     </div>
