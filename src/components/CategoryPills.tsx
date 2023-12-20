9ac972488d42f1df9d9f6b67f3f9e3071f3c8105
Darko <dj_mamlyooo@hotmail.com>
2023-12-20 23:49:27 +0100

    Buttons in CategoryPills dinamicly refactored

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,11 +1,22 @@
 import Button from "./Button";
 
-const CategoryPills = () => {
+type CategoryPillProps = {
+  categories: string[]
+}
+
+const CategoryPills = ({ categories }: CategoryPillProps) => {
   return (
     <div className="overflow-x-hodden relative">
       <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
-        <Button variant="dark" className="py-1 px-3 rounded-lg whitespace-nowrap">All</Button>
-        <Button className="py-1 px-3 rounded-lg whitespace-nowrap">JavaScript</Button>
+      { categories.map(category => (
+        <Button
+        key={category}
+          variant="dark"
+          className="py-1 px-3 rounded-lg whitespace-nowrap"
+        >
+          {category}
+        </Button>
+        )) }
       </div>
     </div>
   );
